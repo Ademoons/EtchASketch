9fed87e70a7c351ab86abce00a14e60e81048c70
Ade <gbli@protonmail.com>
2020-11-20 00:20:05 +0000

    needs final grid container size fix and black mouseover effect

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
-const etcha = document.getElementById("etcha");
+const etcha = document.getElementById('etcha');
+const box = document.getElementById('box');
+
+let gridSize = document.querySelector('#input');
+
+let clean = document.getElementById('reset').addEventListener('click', resetPad);
+let erase = document.getElementById('erase').addEventListener('click', erasePad);
+let change = document.getElementById('submit').addEventListener('click', changeRows);
+
+/* cool effect just not on this project
+box.addEventListener('mousemove', runEvent);
+
+function runEvent(e) {
+    
+    document.body.style.backgroundColor = "rgba(56, "+e.offsetX+", "+e.offsetY+", 0.2)";
+}
+*/
 
 function makeRows(rows, columns) {
     etcha.style.setProperty('--grid-rows', rows);
     etcha.style.setProperty('--grid-columns', columns);
-    for (i = 0; i < (rows * columns); i++) {
+    for (let i = 0; i < (rows * columns); i++) {
         let cell = document.createElement("div");
-        cell.innerText = (i + 1);
+        cell.style.background = "#fefefe";
+        cell.addEventListener("mouseenter", cellColor);
         etcha.appendChild(cell).className = "grid-item"; 
     }
 }
 
-makeRows(4, 4);
+function changeRows() {
+    etcha.innerHTML = "";
+    makeRows(gridSize.value, gridSize.value);
+}
 
-function clean() {
+function resetPad() {
+    etcha.innerHTML = "";
     makeRows(16, 16);
 }
+
+function erasePad() {
+    etcha.innerHTML = "";
+    if (gridSize.value == 16) {
+        makeRows(16, 16);
+    } else {
+        changeRows();
+    }
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * 255);
+  }
+  
+  function cellColor(e) {
+    return e.target.style.background = `rgb(${randomNumber()}, 
+      ${randomNumber()}, ${randomNumber()}) `;
+  }
+
+  makeRows(16, 16);
